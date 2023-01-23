e25b8715c314562ae5793d2c9875061414080068
ChitraChaudhari <chitraksonawane@gmail.com>
2023-01-23 12:38:23 -0500

    Defining an end point for API

--- testDeployment/app.py
+++ testDeployment/app.py
@@ -13,24 +13,27 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/', methods= ['GET','POST'])
+model = pickle.load(open("model.pkl",'rb'))
+ 
+@app.route('/')
 def home():
     if(request.method=='GET'):
         data = "Hello World!"
         return jsonify({'data':data})
 
-@app.route('/predict/')
-def price_predict():
-    model = pickle.load(open('model.pickle','rb'))
+@app.route('/predict', methods = ['POST'])
+def predict():
+    #odel = pickle.load(open('model.pkl','rb'))
     income = request.args.get('income')
     house_age = request.args.get('house_age')
     rooms = request.args.get('rooms')
     bedrooms = request.args.get('bedrooms')
     population = request.args.get('population')
     
-    test_df = pd.DataFrame({'Income':[income], 'HouseAge':[house_age], 'Rooms':[rooms],'Bedrooms':[bedrooms],'Population':[population]})
+    test_df = pd.DataFrame({'Avg. Area Income':[income], 'Avg. Area House Age':[house_age], 'Avg. Area Number of Rooms':[rooms],'Avg. Area Number of Bedrooms':[bedrooms],'Area Population':[population]})
     
     pred_price = model.predict(test_df)
+    
     return jsonify({'House Price':str(pred_price)})
 
 #driver function
